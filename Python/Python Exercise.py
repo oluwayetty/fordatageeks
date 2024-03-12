19b6ce78329528e562067571d0ed5f5f86faeb13
Oluwatoyin Koleosho <oluwayetti@gmail.com>
2024-03-12 18:00:20 +0000

    Add files via upload

---
+++ Python/Python Exercise.py
@@ -0,0 +1,66 @@
+# LEVEL ONE
+'''
+Integer and Float:
+Create variables to store an integer and a float.
+Perform arithmetic operations (addition, subtraction, multiplication, - division) on these variables.
+
+String Manipulation:
+Declare a string variable and print its length.
+Concatenate two strings and print the result.
+Extract a substring from a string.
+
+List Handling:
+Create a list of numbers and find the sum and average.
+Append an element to the list and remove an element from it.
+Access specific elements in the list.
+
+
+Loops:
+User Input Validation: Use a while loop to repeatedly prompt the user for input until they enter a valid number.
+'''
+
+# LEVEL TWO
+'''
+Write a Python program to add 'ing' at the end of a given string. 
+If the given string already ends with 'ing' then add 'ly' instead. 
+If the string length of the given string is less than 3, leave it unchanged.
+Sample String : 'abc' Expected Result : 'abcing' Sample String : 'string' Expected Result : 'stringly'
+'''
+
+
+'''
+Create a variable of a list of numbers
+Write a python program that returns a list
+of the square of each number in the list you created.
+E.g 
+list_of_nums = [1,4,2]
+The program should print [1,16,4]
+
+OPTIONAL EXTENSION: Write another program with same list but 
+this time around, the program should return a list of
+odd numbers in the list
+'''
+
+'''
+You are given a list of dictionaries representing information about employees.
+Each dictionary contains the employee's name, salary, and department.
+Write a python program called that takes the list of dictionaries.
+The program should return the name of the employee with the highest salary 
+
+employees = [
+    {'name': 'Alice', 'salary': 60000, 'department': 'HR'},
+    {'name': 'Bob', 'salary': 75000, 'department': 'Engineering'},
+    {'name': 'Charlie', 'salary': 80000, 'department': 'Engineering'},
+    {'name': 'David', 'salary': 70000, 'department': 'HR'},
+    {'name': 'Eva', 'salary': 90000, 'department': 'Marketing'}
+]
+In this example, the program should return 'Eva'
+'''
+
+'''
+Write a python program that checks if
+a user input is a palindrome, if it is, it says 'It is a palindrome'
+otherwise, it tell thems to input a palindrome.
+A palindrome is word, phrase, or sequence that reads the same backwards as forwards,
+e.g. madam, 112211.
+'''
